4e27f85f6e18d4fe0015939111565e651f2b5956
devonjonest <devonjonest@gmail.com>
2024-09-23 15:20:20 -0400

    DOTS Lab

--- starter/level-three.js
+++ starter/level-three.js
@@ -2,3 +2,35 @@
  * DOTS: Level Three
  *
  */
+
+
+// Needed help from ChatGPT to figured out the Data Increments.
+// Still not really sure how the increments were determined.
+let score = 0;
+
+const balls = document.querySelectorAll('.js-ball');
+const scoreDisplay = document.querySelector('.js-score');
+const levelWinner = document.querySelector('.level-winner');
+
+console.log(balls);
+
+balls.forEach(ball => {
+    ball.addEventListener('click', () => {
+        console.log(ball.dataset);
+
+        const incrementValue = parseInt(ball.getAttribute('data-increment'), 10);
+        console.log(`Increment Value: ${incrementValue}`);
+
+        score += incrementValue; 
+        console.log(`Score: ${score}`);
+
+        scoreDisplay.innerText = score;
+        console.log(`Score Display: ${scoreDisplay.innerText}`);
+
+        if (score >= 100) {
+            console.log('Level complete!');
+
+            levelWinner.style.opacity = 1;
+        }
+    })
+})
--- starter/level-two.js
+++ starter/level-two.js
@@ -2,3 +2,26 @@
  * DOTS: Level Two
  *
  */
+let score = 0;
+
+const balls = document.querySelectorAll('.js-ball');
+const scoreDisplay = document.querySelector('.js-score');
+const levelWinner = document.querySelector('.level-winner');
+
+console.log(balls);
+
+balls.forEach(ball => {
+    ball.addEventListener ('click', () => {
+        score += 10;
+        console.log(`Score: ${score}`);
+
+        scoreDisplay.innerText = score;
+        console.log(`Score Display: ${scoreDisplay.innerText}`);
+
+        if (score >= 100) {
+            console.log('Level complete!');
+
+            levelWinner.style.opacity = 1;
+        }
+    })
+})
